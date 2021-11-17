717dc56a45fa695fc9a5db82372cd1e28253a711
sabilimaulana <spacysbl.learn@gmail.com>
2021-11-17 19:38:03 +0700

    Integration : Membuat API data types

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
+import { GameItemTypes } from "../../../services/data-types";
 import { getFeaturedGame } from "../../../services/player";
 import GameItem from "../../molecules/GameItem";
 
@@ -26,12 +27,12 @@ function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          {gameList.map((game) => (
+          {gameList.map((game: GameItemTypes) => (
             <GameItem
               key={game?._id}
               title={game?.name}
               category={game?.category?.name}
-              thumbnail={`https://sbl-store-gg.herokuapp.com/uploads/${game.thumbnail}`}
+              thumbnail={`${process.env.NEXT_PUBLIC_IMG}/${game.thumbnail}`}
             />
           ))}
         </div>
