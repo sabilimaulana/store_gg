f4638e9a670ed79f39745df5073886990a27eb74
sabilimaulana <spacysbl.learn@gmail.com>
2021-11-21 19:46:12 +0700

    Integration : Setup Sign-Up Form

--- components/organisms/SignUpForm/index.tsx
+++ components/organisms/SignUpForm/index.tsx
@@ -1,69 +1,85 @@
+import { FormEvent, useState } from "react";
+import cx from "classnames";
+import { useRouter } from "next/router";
+
 function SignUpForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const className = {
+    label: cx("form-label text-lg fw-medium color-palette-1 mb-10"),
+    input: cx("form-control rounded-pill text-lg"),
+  };
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const userForm = { name, email, password };
+    localStorage.setItem("user-form", JSON.stringify(userForm));
+    router.push("sign-up-photo");
+  };
+
   return (
-    <>
+    <form onSubmit={onSubmit}>
       <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign Up</h2>
       <p className="text-lg color-palette-1 m-0">
         Daftar dan bergabung dengan kami
       </p>
       <div className="pt-50">
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label
-          htmlFor="name"
-          className="form-label text-lg fw-medium color-palette-1 mb-10"
-        >
+        <label htmlFor="name" className={className.label}>
           Full Name
         </label>
         <input
           type="text"
-          className="form-control rounded-pill text-lg"
+          className={className.input}
           id="name"
-          name="name"
           aria-describedby="name"
           placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
         />
       </div>
       <div className="pt-30">
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label
-          htmlFor="email"
-          className="form-label text-lg fw-medium color-palette-1 mb-10"
-        >
+        <label htmlFor="email" className={className.label}>
           Email Address
         </label>
         <input
           type="email"
-          className="form-control rounded-pill text-lg"
+          className={className.input}
           id="email"
-          name="email"
           aria-describedby="email"
           placeholder="Enter your email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
         />
       </div>
       <div className="pt-30">
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label
-          htmlFor="password"
-          className="form-label text-lg fw-medium color-palette-1 mb-10"
-        >
+        <label htmlFor="password" className={className.label}>
           Password
         </label>
         <input
           type="password"
-          className="form-control rounded-pill text-lg"
+          className={className.input}
           id="password"
-          name="password"
           aria-describedby="password"
           placeholder="Your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
       </div>
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <a
+        <button
           className="btn btn-sign-up fw-medium text-lg text-white rounded-pill mb-16"
-          href="/sign-up-photo"
-          role="button"
+          type="submit"
         >
           Continue
-        </a>
+        </button>
 
         <a
           className="btn btn-sign-in fw-medium text-lg color-palette-1 rounded-pill"
@@ -73,7 +89,7 @@ function SignUpForm() {
           Sign In
         </a>
       </div>
-    </>
+    </form>
   );
 }
 
--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -6,16 +6,14 @@ function SignUp() {
   return (
     <section className="sign-up mx-auto pt-lg-100 pb-lg-100 pt-30 pb-47">
       <div className="container mx-auto">
-        <form action="">
-          <div className="pb-50">
-            <Link href="/">
-              <a className="navbar-brand">
-                <Image src="/icon/logo.svg" width={60} height={60} alt="logo" />
-              </a>
-            </Link>
-          </div>
-          <SignUpForm />
-        </form>
+        <div className="pb-50">
+          <Link href="/">
+            <a className="navbar-brand">
+              <Image src="/icon/logo.svg" width={60} height={60} alt="logo" />
+            </a>
+          </Link>
+        </div>
+        <SignUpForm />
       </div>
     </section>
   );
