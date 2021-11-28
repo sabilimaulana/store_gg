bbebe6dec88a3277dc552c9f5a93cf7801ef3990
sabilimaulana <spacysbl.learn@gmail.com>
2021-11-28 20:19:13 +0700

    Integration : Integrasi Game Item

--- components/organisms/CheckoutItem/index.tsx
+++ components/organisms/CheckoutItem/index.tsx
@@ -1,18 +1,38 @@
+import { DetailVoucherTypes } from "@services/data-types";
+import { useEffect, useState } from "react";
+
 function CheckoutItem() {
+  const [dataItem, setDataItem] = useState<DetailVoucherTypes>({
+    _id: "",
+    name: "",
+    status: "",
+    category: { name: "", _id: "" },
+    isFeatured: false,
+    nominals: [],
+    thumbnail: "",
+    user: { _id: "", name: "", avatar: "", email: "", username: "" },
+  });
+
+  useEffect(() => {
+    const dataItemLocal: DetailVoucherTypes = JSON.parse(
+      localStorage.getItem("data-item") || "{}"
+    );
+
+    setDataItem(dataItemLocal);
+  }, []);
+
   return (
     <div className="game-checkout d-flex flex-row align-items-center pt-md-50 pb-md-50 pt-30 pb-30">
       <div className="pe-4">
         <div className="cropped">
-          <img src="/img/Thumbnail-3.png" className="img-fluid" alt="" />
+          {dataItem.thumbnail && (
+            <img src={dataItem.thumbnail} className="img-fluid" alt="Game" />
+          )}
         </div>
       </div>
       <div>
-        <p className="fw-bold text-xl color-palette-1 mb-10">
-          Mobile Legends:
-          <br />
-          The New Battle 2021
-        </p>
-        <p className="color-palette-2 m-0">Category: Mobile</p>
+        <p className="fw-bold text-xl color-palette-1 mb-10">{dataItem.name}</p>
+        <p className="color-palette-2 m-0">{`Category: ${dataItem.category.name}`}</p>
       </div>
     </div>
   );
